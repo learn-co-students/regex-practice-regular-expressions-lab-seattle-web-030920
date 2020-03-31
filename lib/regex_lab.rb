88f48cc1f6828f63ea32e3d6200d26466c86feef
github username <github email address>
2020-03-31 12:52:10 -0700

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,25 @@
 def starts_with_a_vowel?(word)
-
+    !!word.match(/^[AaEeIiOoUu]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    # words = text.split(" ")
+    # words.select do |word|
+    #     !!word.match(/^un/) && !!word.match(/ing$/)
+    # end
+    un_words = text.scan(/un\w+/)
+    un_words.grep(/\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+    words = text.split(" ")
+    words.grep(/^\w{5}$/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    !!text.match(/^[A-Z]/) && !!text.match(/[.!?]$/)
 end
 
 def valid_phone_number?(phone)
-
+    !!phone.match(/([0-9] *){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
 end
